cd34f56f7c40966cf1393f5fc428b494e3b5332b
Styp Canto <styp611@outlook.com>
2025-01-31 03:38:30 -0500

    Creando nuevos estilos en el portal de datos parte 2

--- src/components/Nav/Nav_transversal.tsx
+++ src/components/Nav/Nav_transversal.tsx
@@ -4,61 +4,61 @@ const NavTransversal = () => {
   return (
     <div className="w-64 bg-white shadow-md py-4 px-6">
       <h2 className="text-xl font-bold text-[#2e63a6] mb-8">DOCUMENTOS TRANSVERSALES</h2>
-      <ul className="space-y-4">
+      <ul className="space-y-6">
         <li>
           <Link
             to="/normativa-legal"
-            className="block text-lg font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
+            className="block  font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6]  py-2 px-4 rounded transition duration-300"
           >
-            NORMATIVA LEGAL
+            Normativa Legal
           </Link>
         </li>
         <li>
           <Link
             to="/organigrama"
-            className="block text-lg font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
+            className="block  font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
           >
-            ORGANIGRAMA
+            Organigrama
           </Link>
         </li>
         <li>
           <Link
             to="/mapa-de-procesos"
-            className="block text-lg font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
+            className="block  font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
           >
-            MAPA DE PROCESOS
+           Mapa de Procesos
           </Link>
         </li>
         <li>
           <Link
             to="/politica-de-calidad"
-            className="block text-lg font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
+            className="block  font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
           >
-            POLITICA DE CALIDAD
+            Política de Calidad
           </Link>
         </li>
         <li>
           <Link
             to="/riesgo-y-oportunidades"
-            className="block text-lg font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
+            className="block font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
           >
-            RIESGO Y OPORTUNIDADES
+            Riesgo y Oportunidades
           </Link>
         </li>
         <li>
           <Link
             to="/join-commission"
-            className="block text-lg font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
+            className="block  font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
           >
-            JOIN COMMISSION
+         Join Commission
           </Link>
         </li>
         <li>
           <Link
             to="/power-bi-indicadores"
-            className="block text-lg font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
+            className="block  font-semibold text-gray-700 hover:text-white hover:bg-[#2e63a6] py-2 px-4 rounded transition duration-300"
           >
-            POWER BI INDICADORES
+            Power BI Indicadores
           </Link>
         </li>
       </ul>
